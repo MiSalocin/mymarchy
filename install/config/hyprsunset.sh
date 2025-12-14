7a335d892f7283aa48e8f058d9ed2af331561f82
MiSalocin <misalocin@pm.me>
2025-12-13 22:50:26 -0300

    Less docker and hyprsunset fix
    Removed more code relating to docker, more migrations, and, as UWSM is used, set hyprsunset to start as a systemctl service insetead of a hyprland exec-once

---
+++ install/config/hyprsunset.sh
@@ -0,0 +1,2 @@
+# Enable hyprsunset as a user service, as UWSM is being used
+systemctl --user enable --now hyprsunset.service
--- migrations/waybar-icons.sh
+++
@@ -1,7 +0,0 @@
-echo "Using 󰂱 icon for bluetooth when connected"
-sed -i 's/"format-connected": "",/"format-connected": "󰂱",/' ~/.config/waybar/config.jsonc
-
-echo "Replace Waybar dock icon with something more obvious"
-sed -i 's/"format": ""/"format": ""/' ~/.config/waybar/config.jsonc
-sed -i '/#custom-expand-icon {/,/}/ s/margin-right: 20px;/margin-right: 18px;/' ~/.config/waybar/style.css
-myarchy-restart-waybar
